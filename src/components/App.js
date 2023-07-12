5f98ea8bbd40577fd8abdbc6599f5da65778f50e
Tin Cu Kang <cukang98@gmail.com>
2023-07-12 15:08:29 +0800

    CSS layout adjustment

--- src/components/App.js
+++ src/components/App.js
@@ -1,15 +1,18 @@
-import { CssBaseline } from '@mui/material';
-import { Routes, Route } from 'react-router-dom';
-import { Movies } from './';
-import React, { useRef } from 'react'
-import './global.css'
+import { CssBaseline } from "@mui/material";
+import { Routes, Route } from "react-router-dom";
+import { Movies } from "./";
+import React, { useRef } from "react";
+import "./global.css";
 
 function App() {
   return (
     <div className="root">
-      <Routes>
-        <Route exact path= '/' element={<Movies />} /> 
-      </Routes>
+      <main className="content">
+      <div className='toolkit' />
+        <Routes>
+          <Route exact path="/" element={<Movies />} />
+        </Routes>
+      </main>
     </div>
   );
 }
